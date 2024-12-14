189aabed2f46dfda4bd4fee92005ded5b51d7e32
Your Name <you@example.com>
2024-12-14 11:00:45 +0700

    xa

--- db.js
+++ db.js
@@ -2,11 +2,14 @@ const mysql = require('mysql2/promise');
 
 // Konfigurasi koneksi pooling
 const pool = mysql.createPool({
-  host: '142.132.150.169',
-  user: 'ucxfiycw_sipremium',
-  password: 'igzUR9GrENcvqr@',
-  database: 'ucxfiycw_sipremium',
-  waitForConnections: true,s
+  host: '139.59.109.210',
+  port: 3306,
+  user: 'ccgnimex',
+  password: 'aaaaaaac',
+  database: 'ccgnimex',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 // Fungsi untuk mendapatkan koneksi dari pool
